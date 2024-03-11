eb856e8bde7768fca994a7e8648fe88c91c9c37e
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 18:42:26 +0530

    "new"

--- src/LudoBoard2.jsx
+++ src/LudoBoard2.jsx
@@ -9,23 +9,38 @@ export default function Ludoboard2() {
         red: 0,
         green: 0
     }
-    let [moves, setMoves] = useState(ob)
+    let [moves, setMoves] = useState(ob);
 
-      // now initially moves <= ob i.e ob is copied to moves
-      
+    // now initially moves <= ob i.e ob is copied to moves
 
-    let handleblue=()=>{
-        setMoves((bluemove)=>{
-            bluemove:moves.blue+1;
-        })
-    }
+
+
+    // let handleblue = () => {
+    //     setMoves((bluemove) => {
+    //         return { ...bluemove, blue: bluemove.blue + 1 }
+    //     });
+    // }
     // Expectations:=>  Above (bluemoves) should refer to object in moves and update their value i.e moves.blue by 1
     // But in reality Nothing happens. Because in JS when we update any value in an object then only value changes. But there is no change in address of object.
     // We know that useState() works or re-renders only if there is change in object.
     //Now to do so when there is a change in value of object, we will copy it into another object by using ...operator is spread operator.
 
+    //we can aslo write as folloes
 
-  
+    let handleblue = () => {
+        setMoves({ ...moves, blue: moves.blue + 1 });
+    };
+
+    let handleyellow = () => {
+        setMoves({ ...moves, yellow: moves.yellow + 1 });
+    };
+    let handlered = () => {
+        setMoves({ ...moves, red: moves.red + 1 });
+    };
+    let handlegreen = () => {
+        setMoves({ ...moves, green: moves.green+ 1 });
+    };
+
 
     return (
         <div>
@@ -35,13 +50,13 @@ export default function Ludoboard2() {
             <button onClick={handleblue} style={{ backgroundColor: "blue" }}> 1+ </button>
 
             <h3>Yellow = {moves.yellow}</h3>
-            <button style={{ backgroundColor: "yellow", color: "black" }}>1+</button>
+            <button onClick={handleyellow}  style={{ backgroundColor: "yellow", color: "black" }}>1+</button>
 
             <h3>Red = {moves.red}</h3>
-            <button style={{ backgroundColor: "red" }}> 1+</button>
+            <button onClick={handlered} style={{ backgroundColor: "red" }}> 1+</button>
 
             <h3>Green = {moves.green}</h3>
-            <button style={{ backgroundColor: "green" }}>1+</button>
+            <button onClick={handlegreen} style={{ backgroundColor: "green" }}>1+</button>
         </div>
     )
 }
