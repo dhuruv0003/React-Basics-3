3923dc5498f447856076108ced2e780c85b81e00
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 17:35:06 +0530

    "commit"

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -1,7 +1,16 @@
 export default function LudoBoard(){
+    
     return (
         <div className="">
             <h1>Game Begins</h1>
+            <h3>Blue = </h3>
+            <button style={{backgroundColor:"blue"}}>1+</button>
+            <h3>Yellow = </h3>
+            <button style={{backgroundColor:"blue"}}>1+</button>
+            <h3>Red = </h3>
+            <button style={{backgroundColor:"blue"}}> 1+</button>
+            <h3>Green = </h3>
+            <button style={{backgroundColor:"blue"}}>1+</button>
         </div>
     );
 }
