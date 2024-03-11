b56fd276779c4125bd85a3c7620c3e597c60472b
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 19:01:31 +0530

    "new commit"

--- src/LudoBoard2.jsx
+++ src/LudoBoard2.jsx
@@ -13,34 +13,40 @@ export default function Ludoboard2() {
 
     // now initially moves <= ob i.e ob is copied to moves
 
+    // let handleblue = () => {
+    //     setMoves((bluemove) => {
+    //         return { blue: bluemove.blue + 1 }
+    //     });
+    // }
 
+    // Expectations:=>  Above (bluemoves) should refer to object in moves and update their value i.e moves.blue by 1
+    // But in reality Nothing happens. Because in JS when we update any value in an object then only value changes. But there is no change in address of object.
+    // We know that useState() works or re-renders only if there is change in object.
+    //Now to do so when there is a change in value of object, we will copy it into another object by using ...operator is spread operator.
 
     let handleblue = () => {
         setMoves((newmove) => {
             return { ...newmove, blue: newmove.blue + 1 }
         });
     }
-    // Expectations:=>  Above (bluemoves) should refer to object in moves and update their value i.e moves.blue by 1
-    // But in reality Nothing happens. Because in JS when we update any value in an object then only value changes. But there is no change in address of object.
-    // We know that useState() works or re-renders only if there is change in object.
-    //Now to do so when there is a change in value of object, we will copy it into another object by using ...operator is spread operator.
+    // All changes will take place in copied newmove object and then returned to original newmove object.
 
     //we can aslo write as folloes
     // passing updated object inside setmoves
 
-    
+
     let handleyellow = () => {
         setMoves({ ...moves, yellow: moves.yellow + 1 });
     };
-    
-    let handlered=()=>{
-        setMoves((redmove)=>{
-            return {...redmove, red:redmove.red+1}
+
+    let handlered = () => {
+        setMoves((redmove) => {
+            return { ...redmove, red: redmove.red + 1 }
         })
     }
 
-    let handlegreen=()=>{
-        setMoves({...moves,green:moves.green+1})
+    let handlegreen = () => {
+        setMoves({ ...moves, green: moves.green + 1 })
     }
 
 
@@ -52,7 +58,7 @@ export default function Ludoboard2() {
             <button onClick={handleblue} style={{ backgroundColor: "blue" }}> 1+ </button>
 
             <h3>Yellow = {moves.yellow}</h3>
-            <button onClick={handleyellow}  style={{ backgroundColor: "yellow", color: "black" }}>1+</button>
+            <button onClick={handleyellow} style={{ backgroundColor: "yellow", color: "black" }}>1+</button>
 
             <h3>Red = {moves.red}</h3>
             <button onClick={handlered} style={{ backgroundColor: "red" }}> 1+</button>
