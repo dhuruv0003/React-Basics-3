b3dbd51c77d2ded81e008e28aabe55f5dab3d4db
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 17:16:41 +0530

    "new commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import LudoBoard from './LudoBoard'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <LudoBoard></LudoBoard>
     </>
   )
 }
---
+++ src/LudoBoard.jsx
@@ -0,0 +1,7 @@
+export default function LudoBoard(){
+    return (
+        <div className="">
+            <h1>Game Begins</h1>
+        </div>
+    );
+}
