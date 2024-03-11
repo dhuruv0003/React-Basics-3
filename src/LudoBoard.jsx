66436c2ba932c2338137bf0a721f92d836d89b27
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 17:46:21 +0530

    "new commit"

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -1,16 +1,45 @@
+
+// Ludo Board Without using object 
+
+
+import { useState } from "react";
 export default function LudoBoard(){
-    
+ let [count1,setcount1]=useState(0)
+ let handleblue=()=>{
+    setcount1((count)=>{
+        return count+1
+    })
+ }
+ let [count2,setcount2]=useState(0)
+ let handleyellow=()=>{
+    setcount2((count)=>{
+        return count+1
+    })
+ }
+ let [count3,setcount3]=useState(0)
+ let handlered=()=>{
+    setcount3((count)=>{
+        return count+1
+    })
+ }
+ let [count4,setcount4]=useState(0)
+ let handlegreen=()=>{
+    setcount4((count)=>{
+        return count+1
+    })
+ }
+
     return (
         <div className="">
             <h1>Game Begins</h1>
-            <h3>Blue = </h3>
-            <button style={{backgroundColor:"blue"}}>1+</button>
-            <h3>Yellow = </h3>
-            <button style={{backgroundColor:"blue"}}>1+</button>
-            <h3>Red = </h3>
-            <button style={{backgroundColor:"blue"}}> 1+</button>
-            <h3>Green = </h3>
-            <button style={{backgroundColor:"blue"}}>1+</button>
+            <h3>Blue = {count1}</h3>
+            <button onClick={handleblue} style={{backgroundColor:"blue"}}>1+</button>
+            <h3>Yellow = {count2}</h3>
+            <button onClick={handleyellow} style={{backgroundColor:"yellow", color:"black"}}>1+</button>
+            <h3>Red = {count3} </h3>
+            <button onClick={handlered} style={{backgroundColor:"red"}}> 1+</button>
+            <h3>Green = {count4}</h3>
+            <button onClick={handlegreen} style={{backgroundColor:"green"}}>1+</button>
         </div>
     );
 }
